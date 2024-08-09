a7407eee2a928243aaa01098293412d8e43663a8
Roibos22 <grimmeisen.leon@gmail.com>
2024-08-09 16:40:03 +0200

    feat: remove required option from player name fields and assign default name instead

--- src/js/app.js
+++ src/js/app.js
@@ -5,13 +5,13 @@ document.addEventListener('DOMContentLoaded', () => {
 
 	playerForm.addEventListener('submit', (e) => {
 		e.preventDefault();
-		const player1 = document.getElementById('player1').value;
-		const player2 = document.getElementById('player2').value;
+		let player1 = document.getElementById('player1').value.trim();
+		if (!player1) player1 = "Player 1";
+		let player2 = document.getElementById('player2').value.trim();
+		if (!player2) player2 = "Player 2";
 
-		if (player1 && player2) {
-			gameSetup.style.display = 'none';
-			canvas.style.display = 'block';			
-			console.log(`Game started with ${player1} and ${player2}`);
-		}
+		gameSetup.style.display = 'none';
+		canvas.style.display = 'block';			
+		console.log(`Game started with ${player1} and ${player2}`);
 	});
 });
