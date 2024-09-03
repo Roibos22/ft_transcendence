4cd8357472d84caa30be7e7b4ff9fb106ce1915c
JalbersCode <josephal@hotmail.de>
2024-09-03 18:25:35 +0200

    wagwan

--- server/game/urls.py
+++ server/game/urls.py
@@ -3,5 +3,6 @@ from .views import *
 
 urlpatterns = [
     path('', get_games, name='get_games'),
-    path('create/', create_game, name='create_game')
+    path('create/', create_game, name='create_game'),
+    path('join/', join_game, name='join_game')
 ]
--- server/game/views.py
+++ server/game/views.py
@@ -2,7 +2,11 @@ from rest_framework.decorators import api_view
 from rest_framework.response import Response
 from rest_framework import status
 from .models import Game
-from .serializer import GameSerializer
+from .serializer import GameSerializer 
+from django.views import View
+from django.http import JsonResponse
+from django.contrib.auth import authenticate
+from .src.matchmaking import matchmaker
 
 @api_view(['GET'])
 def get_games(request):
@@ -17,3 +21,19 @@ def create_game(request):
         serializer.save()
         return Response(serializer.data, status=status.HTTP_201_CREATED)
     return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+@api_view(['POST'])
+def join_game(request):
+    username = request.data.get('username')
+    password = request.data.get('password')
+
+    user = authenticate(request, username=username, password=password)
+    if user is not None:
+        game_id = matchmaker.add_to_queue(user)
+        if game_id:
+            return Response({'status': 'matched', 'game_id': game_id})
+        else:
+            return Response({'status': 'waiting'})
+    else:
+        return Response({'status': 'error', 'message': 'Invalid credentials'}, status=401)
+
