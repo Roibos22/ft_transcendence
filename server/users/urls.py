172c48b488a271b867df4dd787e5407c6fef30f8
Dilshod Davletyarov <dilshod95@gmail.com>
2024-09-06 20:38:22 +0200

    fix

--- server/users/urls.py
+++ server/users/urls.py
@@ -7,7 +7,7 @@ urlpatterns = [
     path('', get_users, name='get_users'),
     path('create/', create_user, name='create_user'),
     path('login/', user_login, name='login'),
-    path('emailverification/<int:user_id>/', verify_email, name='verify_email'),
+    path('emailverification/<int:user_id>/', verify_email, name='verify_email'), # on email link press
     # Urls pre-2FA login
     path('2fa/verify/', verify_2fa, name='verify_2fa'),
     path('login/2fa/', confirm_2fa, name='confirm_2fa'),
@@ -15,8 +15,8 @@ urlpatterns = [
     path('2fa/setup/', setup_2fa, name = 'setup_2fa'),
     path('login/refresh/', TokenRefreshView.as_view(), name='login_refresh'),
     path('profile/<int:user_id>/', user_profile, name='profile'),
-    path('profile/<int:user_id>/delete/', delete_user, name='delete_user'),
-	path('profile/<int:user_id>/update/', update_user, name='update_user')
+    path('profile/<int:user_id>/delete/', delete_user, name='delete_user'), # I actually don't need user_id?
+	path('profile/<int:user_id>/update/', update_user, name='update_user') # I actually don't need user_id?
 
     # path('o/', include(oauth2_urls)),
 ]
