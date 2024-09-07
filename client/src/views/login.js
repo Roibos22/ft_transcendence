029e97fcb9f3942da6db1de8ce7cf4e3e1505539
LofiLeon <grimmeisen.leon@gmail.com>
2024-09-07 14:22:56 +0200

    feat: implement loginUser call

--- client/src/views/login.js
+++ client/src/views/login.js
@@ -5,12 +5,20 @@ export async function initLoginView() {
 	document.getElementById('app').innerHTML = content;
 
 	const loginForm = document.getElementById('loginForm');
+	const loginError = document.getElementById('loginError');
+
 	if (loginForm) {
-		loginForm.addEventListener('submit', function(e) {
+		loginForm.addEventListener('submit', async function(e) {
 			e.preventDefault();
 			const username = document.getElementById('username').value;
-			window.history.pushState({}, "", "/game-setup");
-			urlLocationHandler();
+			const password = document.getElementById('password').value;
+			
+			const loginSuccess = await loginUser(username, password);
+			
+			if (loginSuccess) {
+				window.history.pushState({}, "", "/game-setup");
+				urlLocationHandler();
+			}
 		});
 	}
 
@@ -22,4 +30,56 @@ export async function initLoginView() {
 			urlLocationHandler();
 		});
 	}
+}
+
+async function loginUser(username, password) {
+	try {
+		const response = await fetch('http://localhost:8000//users/login/', {
+			method: 'POST',
+			headers: {
+				'Content-Type': 'application/json',
+			},
+			body: JSON.stringify({ username, password }),
+		});
+
+		if (!response.ok) {
+			const errorData = await response.json();
+			displayLoginError(errorData);
+			console.log(errorData);
+			throw new Error('Login failed');
+		}
+
+		const data = await response.json();
+		//localStorage.setItem('authToken', data.token);
+		console.log("Login successful");
+		console.log(data);
+		return true;
+	} catch (error) {
+		console.error('Login error:', error);
+		return false;
+	}
+}
+
+function displayLoginError(errorData) {
+	const loginError = document.getElementById('loginError');
+	loginError.style.display = 'block';
+	let errorMessages = [];
+
+	if (errorData.username) {
+		// Username may not be blank
+		errorMessages.push(`Username: ${errorData.username[0]}`);
+	}
+	if (errorData.password) {
+		// Password may not be blank
+		errorMessages.push(`Password: ${errorData.password[0]}`);
+	}
+	if (errorData.detail) {
+		// Password and Username do not match
+		errorMessages.push(errorData.detail);
+	}
+	if (errorMessages.length === 0) {
+		errorMessages.push('An error occurred during login. Please try again.');
+	}
+	
+	loginError.innerHTML = errorMessages.join('<br>');
 }
