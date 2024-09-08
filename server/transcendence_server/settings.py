942ee4913eb6df287b8d2c97022bbcefa5f4dacf
JalbersCode <josephal@hotmail.de>
2024-09-08 14:44:41 +0200

    nothing

--- server/transcendence_server/settings.py
+++ server/transcendence_server/settings.py
@@ -40,6 +40,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'rest_framework',
+    'channels',
 ]
 
 MIDDLEWARE = [
@@ -71,6 +72,7 @@ TEMPLATES = [
 ]
 
 WSGI_APPLICATION = 'transcendence_server.wsgi.application'
+# ASGI_APPLICATION = 'transcendence_server.asgi.application'
 
 
 # Database
