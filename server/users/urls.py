24a96c71561e0223dbe49a7a76be03016488a1e6
ddavlety <ddavlety@student.42berlin.de>
2024-09-09 16:23:53 +0200

    small fix

--- server/users/urls.py
+++ server/users/urls.py
@@ -10,7 +10,7 @@ urlpatterns = [
     path('emailverification/<int:user_id>/', verify_email, name='verify_email'), # on email link press
     # Urls pre-2FA login
     path('2fa/verify/', verify_2fa, name='verify_2fa'),
-    path('login/2fa/', confirm_2fa, name='confirm_2fa'),
+    path('2fa/confirm/', confirm_2fa, name='confirm_2fa'),
     # Urls for logged in user
     path('2fa/setup/', setup_2fa, name = 'setup_2fa'),
     path('login/refresh/', TokenRefreshView.as_view(), name='login_refresh'),
