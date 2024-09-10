8c8c33834a4c5a8745fd398fb3027fd33628e7c1
JalbersCode <josephal@hotmail.de>
2024-09-10 18:24:52 +0200

    middleware for jwt sockets

--- server/game/consumers/matchmaking.py
+++ server/game/consumers/matchmaking.py
@@ -3,24 +3,12 @@ import string
 from channels.generic.websocket import AsyncWebsocketConsumer
 from channels.layers import get_channel_layer
 from asgiref.sync import async_to_sync
-# from game.models import Game
 from channels.db import database_sync_to_async
+import json
+import asyncio
 
-# ************************************** This can  be moved
-# from rest_framework.permissions import BasePermission
-
-# class Is2FAComplete(BasePermission):
-#     """
-#     Custom permission to check if the user has completed 2FA.
-#     Only allows access if 2fa_complete is True in the JWT.
-#     """
-#     def has_permission(self, request):
-#         # Check if '2fa_complete' claim exists in the JWT and is True
-#         if request.user and request.user.is_authenticated:
-#             return getattr(request.user, 'is_2fa_complete', False)
-#         return False
-
-matchmaking_queue = []
+matchmaking_queue_lock = asyncio.Lock()
+matchmaking_queue = {}
 
 class MatchmakingConsumer(AsyncWebsocketConsumer):
     async def connect(self):
@@ -36,26 +24,40 @@ class MatchmakingConsumer(AsyncWebsocketConsumer):
         print("Matchmaking consumer: User Connected!")
         await self.accept()
 
-        matchmaking_queue.append(self)
+        async with matchmaking_queue_lock:
+            
+            if user.id not in matchmaking_queue:
+                matchmaking_queue[user.id] = []
+            matchmaking_queue[user.id].append(self)
 
-        if len(matchmaking_queue) >= 2:
-            player1 = matchmaking_queue.pop(0)
-            player2 = matchmaking_queue.pop(0)
+            if len(matchmaking_queue) >= 2:
+                player1_id, player1_connections = matchmaking_queue.popitem()
+                player2_id, player2_connections = matchmaking_queue.popitem()
 
-            game = await self.create_game(player1.scope['user'], player2.scope['user'])
+                game = await self.create_game(player1_connections[0].scope['user'], player2_connections[0].scope['user'])
 
-            print("Matchmaking created this game: ", game)
+                print("Matchmaking created this game: ", game)
 
-            await player1.send(text_data=str(game.id))
-            await player2.send(text_data=str(game.id))
+                game_data = {
+                    "type": "game_joined",
+                    "game_id": game.id
+                }
 
-            await player1.close()
-            await player2.close()
+                for connection in player1_connections:
+                    await connection.send(text_data=json.dumps(game_data))
+                    await connection.close()
+                for connection in player2_connections:
+                    await connection.send(text_data=json.dumps(game_data))
+                    await connection.close()
 
     async def disconnect(self, close_code):
-        if self in matchmaking_queue:
-            matchmaking_queue.remove(self)
+        user = self.scope['user']
+        if user.id in matchmaking_queue:
+            matchmaking_queue[user.id].remove(self)
+            if len(matchmaking_queue[user.id]) == 0:
+                del matchmaking_queue[user.id]
             print("Player removed form matchmaking queue.")
+        await self.close()
 
     @database_sync_to_async
     def create_game(self, player1, player2):
