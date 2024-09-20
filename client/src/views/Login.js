b6d760ab40f8f6b586a315e7ac4994d4c28e5bde
BarraGit <macmahonbarra+git@gmail.com>
2024-09-20 19:25:40 +0200

    dont ignore case change

---
+++ client/src/views/Login.js
@@ -0,0 +1,84 @@
+import Router from '../router.js';
+import * as Cookies from '../services/cookies.js';
+import * as UserService from '../services/api/userService.js';
+import * as Notification from '../services/notification.js';
+
+export class LoginView {
+	constructor() {
+		this.template = 'login';
+		this.UIelements = null;
+	}
+
+	async init() {
+		const content = await Router.loadTemplate(this.template);
+        document.getElementById('app').innerHTML = content;
+		
+		this.UIelements = this.getUIElements();
+		this.addEventListeners();
+	}
+
+	update() {}
+	
+	getUIElements() {
+		return {
+			loginForm: document.getElementById('loginForm'),
+			showRegistrationLink: document.getElementById('showRegistration'),
+			username: document.getElementById('username'),
+			password: document.getElementById('password')
+		};
+	}
+
+	addEventListeners() {
+		this.UIelements.loginForm.addEventListener('submit', async (e) => {
+			e.preventDefault();
+			await this.loginUser();
+		});
+
+		this.UIelements.showRegistrationLink.addEventListener('click', (e) => {
+			e.preventDefault();
+			window.history.pushState({}, "", "/register");
+			Router.handleLocationChange();
+		});
+	}
+
+	async loginUser() {
+		const username = this.UIelements.username.value;
+		const password = this.UIelements.password.value;
+	
+		try {
+			const response = await UserService.loginUser(username, password);
+	
+			if (response.success) {
+				const data = response.data;
+				Cookies.setCookie("accessToken", data.tokens.access, 24);
+				Cookies.setCookie("refreshToken", data.tokens.refresh, 24);
+				Cookies.setCookie("username", data.username, 24);
+				Notification.showNotification(["Login successful"]);
+				window.history.pushState({}, "", "/game-setup");
+				Router.handleLocationChange();
+			} else {
+				displayLoginError(response.error);
+			}
+		} catch (error) {
+			console.error('Failed to login', error);
+			displayLoginError(error);
+		}
+	}
+	
+	displayLoginError(error) {
+		const loginError = document.getElementById('loginError');
+		loginError.style.display = 'block';
+		let errorMessages = [];
+	
+		if (error.message.includes("400")) {
+			errorMessages.push(`Username and Password field may not be blank.`);
+		} else if (error.message.includes("401")) {
+			errorMessages.push(`Username and Password do not match.`);
+		} else {
+			errorMessages.push(`Something went wrong.`);
+		}
+		errorMessages.push(`Please try again.`);
+		
+		loginError.innerHTML = errorMessages.join('<br>');
+	}
+}
