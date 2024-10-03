6aaf96d785f77db400fff691ac61948946042479
Barra <macmahonbarra+git@gmail.com>
2024-10-03 13:09:07 +0200

    remove need for game mode from input handler

--- client/src/components/InputHandler.js
+++ client/src/components/InputHandler.js
@@ -1,5 +1,3 @@
-import { GameModes } from "../constants.js";
-
 export default class InputHandler {
 	constructor(game) {
 		this.game = game;
@@ -7,20 +5,8 @@ export default class InputHandler {
 	}
 
 	init() {
-		switch(this.game.gameMode) {
-			case GameModes.SINGLE:
-				document.addEventListener('keydown', (e) => this.handleKeyDownSingle(e));
-				document.addEventListener('keyup', (e) => this.handleKeyUpSingle(e));
-				break;
-			case GameModes.MULTI:
-				document.addEventListener('keydown', (e) => this.handleKeyDownMulti(e));
-				document.addEventListener('keyup', (e) => this.handleKeyUpMulti(e));
-				break;
-			case GameModes.ONLINE:
-				document.addEventListener('keydown', (e) => this.handleKeyDownOnline(e));
-				document.addEventListener('keyup', (e) => this.handleKeyUpOnline(e));
-				break;
-		} 
+		document.addEventListener('keydown', (e) => this.handleKeyDown(e));
+		document.addEventListener('keyup', (e) => this.handleKeyUp(e));
 		window.addEventListener('keydown', (e) => this.preventDefaultScroll(e));
 	}
 
@@ -30,117 +16,44 @@ export default class InputHandler {
 		}
 	}
 
-	// SINGLE
-
-	handleKeyDownSingle(e) {
-		switch(e.key) {
-			case 'ArrowUp':
-				this.game.engine.setPlayerDirection(2, -1);
-				break;
-			case 'ArrowDown':
-				this.game.engine.setPlayerDirection(2, 1);
-				break;
-			case 'Enter':
-				//this.handleEnterKey();
-				break;
-
-		}
-	}
-
-	handleKeyUpSingle(e) {
-		switch(e.key) {
-			case 'ArrowUp':
-			case 'ArrowDown':
-				this.game.engine.setPlayerDirection(2, 0);
-				break;
-		}
-	}
-
-	// MULTI
-
-	handleKeyDownMulti(e) {
-		switch(e.key) {
-			case 'w':
-				this.game.engine.setPlayerDirection(1, -1);
-				break;
-			case 's':
-				this.game.engine.setPlayerDirection(1, 1);
-				break;
-			case 'ArrowUp':
-				this.game.engine.setPlayerDirection(2, -1);
-				break;
-			case 'ArrowDown':
-				this.game.engine.setPlayerDirection(2, 1);
-				break;
-			case 'Enter':
-				//this.handleEnterKey();
-				break;
-
-		}
-	}
-
-	handleKeyUpMulti(e) {
-		switch(e.key) {
-			case 'w':
-			case 's':
-				this.game.engine.setPlayerDirection(1, 0);
-				break;
-			case 'ArrowUp':
-			case 'ArrowDown':
-				this.game.engine.setPlayerDirection(2, 0);
-				break;
-		}
-	}
-
-	// ONLINE
-
-	handleKeyDownOnline(e) {
-		if (this.game.socket) {
-			switch(e.key) {
-				case 'ArrowUp':
-					this.game.socket.send(JSON.stringify({ action: 'move_player', direction: '-1' }));
-					break;
-				case 'ArrowDown':
-					this.game.socket.send(JSON.stringify({ action: 'move_player', direction: '1' }));
-					break;
-				case 'Enter':
-					this.game.socket.send(JSON.stringify({ action: 'player_ready' }));
-					break;
-			}
-		} else {
+	handleKeyDown(e) {
+		if (!this.game.socket) {
 			console.log("No Socket Connected");
 		}
+			
+		switch(e.key) {
+			case 'w':
+				this.game.socket.send(JSON.stringify({ action: `move_player`, player_no: '1', direction: '-1' }));
+				break;
+			case 's':
+				this.game.socket.send(JSON.stringify({ action: `move_player`, player_no: '1', direction: '1' }));
+				break;
+			case 'ArrowUp':
+				this.game.socket.send(JSON.stringify({ action: `move_player`, player_no: '2', direction: '-1' }));
+				break;
+			case 'ArrowDown':
+				this.game.socket.send(JSON.stringify({ action: `move_player`, player_no: '2', direction: '1' }));
+				break;
+			case 'Space':
+				this.game.socket.send(JSON.stringify({ action: 'player_ready', player_no: '1' }));
+				break;
+			case 'Enter':
+				this.game.socket.send(JSON.stringify({ action: 'player_ready', player_no: '2' }));
+				break;
+		}
 	}
 
-	handleKeyUpOnline(e) {
-		if (this.game.socket) {
-			switch(e.key) {
-				case 'ArrowUp':
-				case 'ArrowDown':
-					this.game.socket.send(JSON.stringify({
-						action: 'move_player',
-						direction: '0' }));
-					break;
-			}
-		} else {
+	handleKeyUp(e) {
+		if (!this.game.socket) {
 			console.log("No Socket Connected");
 		}
+
+		if (!['w', 's', 'ArrowUp', 'ArrowDown'].includes(e.key)) return;
+
+		const player_no = e.key === 'w' || e.key === 's' ? '1' : '2';
+		this.game.socket.send(JSON.stringify({
+			action: 'move_player',
+			player_no: player_no,
+			direction: '0' }));
 	}
 }
-
-	//handleEnterKey() {
-		// if (this.game.state.waitingForEnter) {
-		// 	if (State.get('gameData', 'phase') === GamePhases.MATCH_ENDED) {
-		// 		incrementCurrentMatchIndex();
-		// 		this.game.State.startNextMatch();
-		// 		this.game.State.startCountdown();
-		// 	} else if (State.get('gameData', 'phase') === GamePhases.FINISHED) {
-		// 		console.log("TOURNAMENT COMPLETED");
-		// 		// Add logic to restart the tournament or return to main menu
-		// 	} else {
-		// 		this.game.State.startCountdown();
-		// 	}
-		// 	this.game.State.waitingForEnter = false;
-		// }
-//	}
-//}
