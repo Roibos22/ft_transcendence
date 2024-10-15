37442d57ce8e5b090978f9abe90bb9ee69c0af32
Roibos22 <grimmeisen.leon@gmail.com>
2024-10-15 17:41:31 +0200

    wip

--- client/src/views/Game.js
+++ client/src/views/Game.js
@@ -15,8 +15,6 @@ export class GameView {
 		const content = await Router.loadTemplate('game');
 		document.getElementById('app').innerHTML = content;
 
-		//await this.getLocalGame();
-
 		const tournament = State.get('tournament');
 		const currentMatchIndex = tournament.currentMatchIndex;
 		const matches = tournament.matches;
@@ -26,8 +24,59 @@ export class GameView {
 	}
 
 	update() {
-		//this.game.update();
-		this.UIManager.update();
+		if (this.UIManager) {
+			this.UIManager.update();
+		}
+		this.updateTournamentData();
+		if (this.checkGameFinished()) {
+			this.handleGameFinished();
+		}
+	}
+
+	// end game
+
+	updateTournamentData() {
+		const gameData = State.get('gameData');
+		const tournament = State.get('tournament');
+		const index = tournament.currentMatchIndex;
+		
+		const matches = tournament.matches;
+		
+		if (matches[index] && matches[index].players) {
+			matches[index].players[0].score = gameData.player1Score;
+			matches[index].players[1].score = gameData.player2Score;
+		}
+		
+		State.data.tournament.matches = matches;
+	}
+
+	checkGameFinished() {
+		const gameData = State.get('gameData');
+		if (gameData.phase === 'game_over') {
+			console.log("Game finished");
+			return true;
+		}
+		return false;
+	}
+
+	handleGameFinished() {
+		console.log("Handling game finish");
+
+		// update match in tournament
+		// navigate back to overview
+		// close socket
+		// update standings
+		// go to next game
+		// prepare next game
+
+		// const gameData = State.get('gameData');
+		// const matchResult = {
+		// 	player1_name: gameData.constants.player1Username,
+		// 	player2_name: gameData.constants.player2Username,
+		// 	player1_score: gameData.player1Score,
+		// 	player2_score: gameData.player2Score
+		// };
+		// this.handleMatchComplete(matchResult);
 	}
 
 
