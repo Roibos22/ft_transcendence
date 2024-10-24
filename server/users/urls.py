3a46ebf5b4352ccceeb3e794ae9818c07a8e50ff
Dilshod Davletyarov <dilshod95@gmail.com>
2024-10-24 15:12:09 +0200

    Changed user_id to username parsing from url

--- server/users/urls.py
+++ server/users/urls.py
@@ -7,16 +7,16 @@ urlpatterns = [
     path('', get_users, name='get_users'),
     path('create/', create_user, name='create_user'),
     path('login/', user_login, name='login'),
-    path('emailverification/<int:user_id>/', verify_email, name='verify_email'), # on email link press
+    path('emailverification/<str:username>/', verify_email, name='verify_email'), # on email link press
     # Urls pre-2FA login
-    path('2fa/verify/<int:user_id>/', verify_2fa, name='verify_2fa'),
+    path('2fa/verify/<str:username>/', verify_2fa, name='verify_2fa'),
     path('2fa/confirm/', confirm_2fa, name='confirm_2fa'),
     # Urls for logged in user
     path('2fa/setup/', setup_2fa, name = 'setup_2fa'),
     path('login/refresh/', TokenRefreshView.as_view(), name='login_refresh'),
     path('profile/<str:username>/', user_profile, name='profile'),
     path('profile/<str:username>/avatar/', user_avatar, name='get_avatar'),
-    path('profile/<int:user_id>/delete/', delete_user, name='delete_user'), # I actually don't need user_id?
-	path('profile/<int:user_id>/update/', update_user, name='update_user') # I actually don't need user_id?
+    path('profile/<str:username>/delete/', delete_user, name='delete_user'), # I actually don't need user_id?
+	path('profile/<str:username>/update/', update_user, name='update_user') # I actually don't need user_id?
 
 ]
